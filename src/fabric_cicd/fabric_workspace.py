da105092bba0b56d6bdeee06ee2a4ec2abb93f15
Jacob Knightley <97691779+JacobKnightley@users.noreply.github.com>
2025-01-31 07:08:55 -0800

    Fixes #76 - Continue on Unpublish failure (#79)
    This pull request includes a change to the error handling mechanism in
    the `_unpublish_item` method of the `fabric_workspace.py` file. The
    change adds a try-except block to catch exceptions during the deletion
    of an item and logs a warning message if an exception occurs.
    
    Error handling improvement:
    
    *
    [`src/fabric_cicd/fabric_workspace.py`](diffhunk://#diff-3bd0f70ed06c7fc7a0f77378aecc6fb108eeb5263161db3b22270f3de4df99c0R454-R458):
    Added a try-except block to handle potential exceptions during the
    deletion of an item and log a warning message with the item type and
    name if an exception occurs.
    
    Co-authored-by: Jacob Knightley <Jacob.Knightley@microsoft.com>

--- src/fabric_cicd/fabric_workspace.py
+++ src/fabric_cicd/fabric_workspace.py
@@ -451,6 +451,8 @@ class FabricWorkspace:
 
         # Delete the item from the workspace
         # https://learn.microsoft.com/en-us/rest/api/fabric/core/items/delete-item
-        self.endpoint.invoke(method="DELETE", url=f"{self.base_api_url}/items/{item_guid}")
-
-        logger.info("Unpublished")
+        try:
+            self.endpoint.invoke(method="DELETE", url=f"{self.base_api_url}/items/{item_guid}")
+            logger.info("Unpublished")
+        except Exception as e:
+            logger.warning(f"Failed to unpublish {item_type} '{item_name}'.  Raw exception: {e}")
