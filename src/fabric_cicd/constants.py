5fb53c4c3b2746bb89a09395640806f42bcdab3b
Joe Muziki <joemuziki@microsoft.com>
2025-04-21 17:15:29 -0400

    v0.1.15 (#252)
    This pull request updates the `fabric-cicd` package to version 0.1.15,
    introduces release notes for the new version, and updates the version
    constant in the codebase.
    
    ### Code Update:
    * Updated the `VERSION` constant in `src/fabric_cicd/constants.py` to
    reflect the new version, 0.1.15.

--- src/fabric_cicd/constants.py
+++ src/fabric_cicd/constants.py
@@ -4,7 +4,7 @@
 """Constants for the fabric-cicd package."""
 
 # General
-VERSION = "0.1.14"
+VERSION = "0.1.15"
 DEFAULT_WORKSPACE_ID = "00000000-0000-0000-0000-000000000000"
 DEFAULT_API_ROOT_URL = "https://api.powerbi.com"
 FEATURE_FLAG = set()
